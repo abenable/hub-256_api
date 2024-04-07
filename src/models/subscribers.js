ea19315fdab0b454299c251edda5dee418490682
abenable <ableabenaitwe@gmail.com>
2024-04-07 14:51:13 +0300

    Add subscriber routes for subscribing and retrieving subscribers

---
+++ src/models/subscribers.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const subscriberSchema = new mongoose.Schema({
+  username: { type: String },
+  email: { type: String, required: true, unique: true },
+  image: { type: String },
+  subscribedAt: { type: Date },
+});
+
+subscriberSchema.pre('save', async function (next) {
+  if (!this.isNew) return next();
+  this.accountCreatedAt = Date.now();
+  next();
+});
+
+export const SubscriberModel = mongoose.model('subscribers', subscriberSchema);
